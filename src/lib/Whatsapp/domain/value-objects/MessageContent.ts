78d92a7c1a2339f18638a5aafc1a5abdb1a2bf15
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-01 12:26:35 -0500

    feat: :sparkles: Add MessageContent value object for Message model

---
+++ src/lib/Whatsapp/domain/value-objects/MessageContent.ts
@@ -0,0 +1,14 @@
+export class MessageContent {
+  public readonly value: string;
+
+  constructor(value: string) {
+    this.validateContent(value);
+    this.value = value;
+  }
+
+  private validateContent(value: string) {
+    if (value.length < 3) {
+      throw new Error("Message content must be at least 3 characters long");
+    }
+  }
+}
