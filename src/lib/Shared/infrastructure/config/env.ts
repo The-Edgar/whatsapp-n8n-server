03d64c6d9eeea280753a84099f07bd7371a34ba7
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-01 10:54:32 -0500

    feat: :lock: Add environment configuration with validation

---
+++ src/lib/Shared/infrastructure/config/env.ts
@@ -0,0 +1,29 @@
+import path from "node:path";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { z } from "zod";
+
+expand(
+  config({
+    path: path.resolve(
+      process.cwd(),
+      process.env.NODE_ENV === "test" ? ".env.test" : ".env",
+    ),
+  }),
+);
+
+const ZodEnvSchema = z.object({
+  NODE_ENV: z.string().min(1).max(12).default("development"),
+  PORT: z.string().min(1).max(5).default("9999"),
+});
+
+const { data: env, error } = ZodEnvSchema.safeParse(process.env);
+
+if (error) {
+  console.error("Invalid env:");
+  console.error(JSON.stringify(error.flatten().fieldErrors, null, 2));
+
+  process.exit(1);
+}
+
+export { env };
