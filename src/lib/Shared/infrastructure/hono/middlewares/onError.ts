ee728e8da73d871134d0b3425492b69a72f17ad4
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-01 11:00:06 -0500

    feat: :sparkles: Add Hono error middleware with custom status handling

---
+++ src/lib/Shared/infrastructure/hono/middlewares/onError.ts
@@ -0,0 +1,25 @@
+import { env } from "@/lib/Shared/infrastructure/config/env";
+import type { ErrorHandler } from "hono";
+import type { ContentfulStatusCode, StatusCode } from "hono/utils/http-status";
+
+import * as HttpStatusCodes from "@/lib/Shared/common/HttpStatusCodes";
+
+const isProd = env?.NODE_ENV === "production";
+
+export const onError: ErrorHandler = (err, c) => {
+  const currentStatus =
+    "status" in err ? (err.status as StatusCode) : c.res.status;
+
+  const statusCode =
+    currentStatus !== HttpStatusCodes.OK
+      ? currentStatus
+      : HttpStatusCodes.INTERNAL_SERVER_ERROR;
+
+  return c.json(
+    {
+      message: err.message,
+      stack: isProd ? undefined : err.stack,
+    },
+    statusCode as ContentfulStatusCode,
+  );
+};
