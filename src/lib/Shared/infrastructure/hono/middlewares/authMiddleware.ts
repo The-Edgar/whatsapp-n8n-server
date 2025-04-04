216955ab746b2da5bc26b21834f4c19f52d9b1ff
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-04-04 14:26:11 -0500

    feat: :sparkles: Add authentication middleware using API key verification

---
+++ src/lib/Shared/infrastructure/hono/middlewares/authMiddleware.ts
@@ -0,0 +1,19 @@
+import { env } from "@/lib/Shared/infrastructure/config/env";
+import type { MiddlewareHandler } from "hono";
+
+import * as HttpStatusCodes from "@/lib/Shared/common/HttpStatusCodes";
+import * as HttpStatusPhrases from "@/lib/Shared/common/HttpStatusPhrases";
+
+export const authMiddleware: MiddlewareHandler = async (c, next) => {
+  const apiKey = c.req.header("x-api-key");
+  console.log(env?.API_KEY);
+
+  if (!apiKey || apiKey !== env?.API_KEY) {
+    return c.json(
+      { message: HttpStatusPhrases.UNAUTHORIZED },
+      HttpStatusCodes.UNAUTHORIZED,
+    );
+  }
+
+  await next();
+};
